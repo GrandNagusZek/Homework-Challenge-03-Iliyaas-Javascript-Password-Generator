5abb9b7eb1dfc9c2b68a36fc7bcc28e40f4ba39a
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-14 02:58:43 -0500

    modified if(confirmLowerCase) statement)

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Homework-Challenge-03-Iliyaas-Javascript-Password-Generator
 
 ## 
-var lowercase="abcdefghijklmnopqrstuvwxyz";
-var uppercase=lowercase.toUpperCase();
-var numeric="0123456789";
-var specialCharacters="!@#$%^&*_-~=+";
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowercase="abcdefghijklmnopqrstuvwxyz";
+var uppercase=lowercase.toUpperCase();
+var numeric="0123456789";
+var specialCharacters="!@#$%^&*_-~=+";
+
 function generatePassword() {
+  var savedPassword="";
   var passwordSize=prompt("Enter password length, between 8 and 128 characters")
-  console.log(passwordSize)
   
 if(passwordSize>= 8 && passwordSize<=128){
     var confirmLowerCase=confirm("Do you want to include lowercase characters in the password?");
-    console.log(confirmLowerCase);
     var confirmUpperCase=confirm("Do you want to include uppercase characters in the password?");
     var confirmNumeric=confirm("Do you want to include numeric characters in the password?");
     var confirmSpecialCharacters=confirm("Do you want to include special characters in the password?");
+    
+
+if(confirmLowerCase){
+     var randomIndex= Math.random() * lowercase.length  // Math.random() * 26 => extends the range from 0 to 1 => 0 to 26 
+}
+
+
 }
 else{ 
   alert("Invalid entry, Password length must be between 8 and 128 characters");
 }
+
+
+
+
+
+
+
+
+
+
+return savedPassword
+
 
 }
 
