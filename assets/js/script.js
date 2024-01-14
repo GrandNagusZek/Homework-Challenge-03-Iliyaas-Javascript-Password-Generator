c94d0b9ae52f74d9606fd7a8d7fc36065c102e9d
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-14 03:23:17 -0500

    modified confirm upper and lower case if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercase="abcdefghijklmnopqrstuvwxyz";
-var uppercase=lowercase.toUpperCase();
-var numeric="0123456789";
-var specialCharacters="!@#$%^&*_-~=+";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = lowercase.toUpperCase();
+var numeric = "0123456789";
+var specialCharacters = "!@#$%^&*_-~=+";
 
 function generatePassword() {
-  var savedPassword="";
-  var passwordSize=prompt("Enter password length, between 8 and 128 characters")
-  
-if(passwordSize>= 8 && passwordSize<=128){
-    var confirmLowerCase=confirm("Do you want to include lowercase characters in the password?");
-    var confirmUpperCase=confirm("Do you want to include uppercase characters in the password?");
-    var confirmNumeric=confirm("Do you want to include numeric characters in the password?");
-    var confirmSpecialCharacters=confirm("Do you want to include special characters in the password?");
-    
+  var savedPassword = "";
+  var passwordSize = prompt("Enter password length, between 8 and 128 characters")
 
-if(confirmLowerCase){
-     var randomIndex= Math.random() * lowercase.length  // Math.random() * 26 => extends the range from 0 to 1 => 0 to 26 
-}
+  if (passwordSize >= 8 && passwordSize <= 128) {
+    var confirmLowerCase = confirm("Do you want to include lowercase characters in the password?");
+    var confirmUpperCase = confirm("Do you want to include uppercase characters in the password?");
+    var confirmNumeric = confirm("Do you want to include numeric characters in the password?");
+    var confirmSpecialCharacters = confirm("Do you want to include special characters in the password?");
 
 
-}
-else{ 
-  alert("Invalid entry, Password length must be between 8 and 128 characters");
-}
+
+    for (var i = 0; i < passwordSize; i++) {
 
 
 
 
+      if (confirmLowerCase && savedPassword.length < passwordSize) {
+        var randomIndex = Math.floor(Math.random() * lowercase.length)  // Math.random() * 26 => extends the range from 0 to 1 => 0 to 26, this includes decimals. Math.floor round it down, ex. 2.8 => 2
+        console.log(randomIndex);
+
+
+        savedPassword = savedPassword + lowercase[randomIndex]
+
+      }
+
+      if (confirmUpperCase && savedPassword.length < passwordSize) {
+        var randomIndex = Math.floor(Math.random() * uppercase.length)  // Math.random() * 26 => extends the range from 0 to 1 => 0 to 26, this includes decimals. Math.floor round it down, ex. 2.8 => 2
+        console.log(randomIndex);
+
+
+        savedPassword = savedPassword + uppercase[randomIndex]
+
+      }
+
+    }
+
+  }
+  else {
+    alert("Invalid entry, Password length must be between 8 and 128 characters");
+  }
 
 
 
 
 
 
-return savedPassword
+  return savedPassword
 
 
 }
